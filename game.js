9fc53be9df5aff5c822f06cc4dce3e197e5ad268
arapau <mariapaulinaar@hotmail.com>
2020-08-21 14:14:36 -0500

    update

--- game.js
+++ game.js
@@ -1,1 +1,30 @@
-game
+var canvas, ctx, x, y, dx, dy, ballRadius;
+
+function setup() {
+  canvas = document.getElementById('myCanvas');
+  ctx = canvas.getContext('2d');
+  x = canvas.width/2;
+  y = canvas.height-30;
+  dx = 2;
+  dy = -2;
+  ballRadius = 10;
+}
+
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, Math.PI*2, false);
+  ctx.fillStyle = '#82589F';
+  ctx.fill();
+  ctx.closePath();
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  x += dx;
+  y += dy;
+}
+
+
+setup()
+setInterval(draw, 10);
